939a787642550cc6ceaabbb8492b7cd1af7d6524
BasakK6 <katukbasak@gmail.com>
2023-02-23 18:36:59 +0300

    feature: added new extensions about size and snackbar

---
+++ lib/core/context_extensions.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+extension ContextExtension on BuildContext {
+  NavigatorState get navigation => Navigator.of(this);
+
+  MediaQueryData get mediaQuery => MediaQuery.of(this);
+
+  ScaffoldMessengerState get scaffoldMessenger => ScaffoldMessenger.of(this);
+}
+
+extension SizeExtension on BuildContext {
+  double get width => mediaQuery.size.width;
+
+  double get height => mediaQuery.size.height;
+
+  double dynamicWidth(double percentage) => width * percentage;
+
+  double dynamicHeight(double percentage) => height * percentage;
+}
+
+extension SnackBarExtension on BuildContext {
+  void showSnackBar(String message) => scaffoldMessenger.showSnackBar(
+        SnackBar(
+          content: Text(message),
+          duration: const Duration(seconds: 2),
+        ),
+      );
+}
--- lib/project/context_extension.dart
+++
@@ -1,5 +0,0 @@
-import 'package:flutter/cupertino.dart';
-
-extension ContextExtension on BuildContext {
-  NavigatorState get navigation => Navigator.of(this);
-}
